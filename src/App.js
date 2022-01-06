23aaa0beea207d895a4278284670b955597d4230
Alex Theoklitou <alex_theo_cy@hotmail.com>
2022-01-06 09:55:25 +0000

    fe setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import axios from 'axios'
 
 function App() {
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get('/api/endpoint')
+      console.log(res.data)
+    }
+    getData()
+  })
+
   return <h1>Hello World</h1>
 }
 
