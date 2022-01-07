ebd5c783a4ced0711c507e750b861ae8f050b05d
Joe Freeman <joefreeman8@gmail.com>
2022-01-07 15:00:21 +0000

    no changes

--- src/components/auth/Account.js
+++ src/components/auth/Account.js
@@ -1,39 +1,32 @@
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-
-
-function Account(userId) {
-=======
-import axios from 'axios'
+// import { Link } from 'react-router-dom'
 
 function Account() {
   localStorage.getItem('token')
 
   console.log(localStorage.getItem('token'))
->>>>>>> development
 
   return (
     <div>
       <div>
         <div>
           <button className="contained">
-            <Link to={`/account/${userId}`}>
+            {/* <Link to={`/account/${userId}`}> */}
               View Account
-            </Link>
+            {/* </Link> */}
           </button>
         </div>
         <div>
           <button className="contained">
-            <Link to={`/account/${userId}/edit`}>
+            {/* <Link to={`/account/${userId}/edit`}> */}
               Edit Account
-            </Link>
+            {/* </Link> */}
           </button>
         </div>
         <div>
           <button className="contained">
-            <Link to={`/account/${userId}`}>
+            {/* <Link to={`/account/${userId}`}> */}
               Delete Account
-            </Link>
+            {/* </Link> */}
           </button>
         </div>
       </div>
