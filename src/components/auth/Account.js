2e7876db7f000a1f11d652c5a988e99030d66960
Joe Freeman <joefreeman8@gmail.com>
2022-01-07 14:43:03 +0000

    account-edits

--- src/components/auth/Account.js
+++ src/components/auth/Account.js
@@ -1,16 +1,31 @@
-function Account() {
+import { Link } from 'react-router-dom'
+
+
+function Account(userId) {
 
   return (
     <div>
       <div>
         <div>
-          <button className="contained">View Profile</button>
+          <button className="contained">
+            <Link to={`/account/${userId}`}>
+              View Account
+            </Link>
+          </button>
         </div>
         <div>
-          <button className="contained">Edit Profile</button>
+          <button className="contained">
+            <Link to={`/account/${userId}/edit`}>
+              Edit Account
+            </Link>
+          </button>
         </div>
         <div>
-          <button className="contained">Delete Profile</button>
+          <button className="contained">
+            <Link to={`/account/${userId}`}>
+              Delete Account
+            </Link>
+          </button>
         </div>
       </div>
     </div>
